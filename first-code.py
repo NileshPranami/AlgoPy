c482918c3e9e16bab659d72a8e96e27206381370
Nilesh <nk966331@gmail.com>
2019-03-17 17:43:15 +0530

    Class check

--- first-code.py
+++ first-code.py
@@ -1,7 +1,18 @@
-def firstFun(*args, **kwargs):
-    print(args)
-    print(kwargs)
+class Employee:
 
-courses = ['Maths', 'Art']
-info = {'name': 'John', 'age': 22}
-firstFun(*courses, **info)
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first+'.'+last+'@gmail.com'
+
+    def name(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp1 = Employee('Nilesh','Pranami',200)
+emp2 = Employee('ram', 'kumar', 322)
+
+print(Employee.name(emp1))
+print(emp1.email)
+print(emp2.email)
