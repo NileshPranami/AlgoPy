9fda3a6a9f7566b42f2a06641035b7388d114bf5
Nilesh <nk966331@gmail.com>
2019-03-23 18:25:47 +0530

    Inheritance completed

--- first-code.py
+++ first-code.py
@@ -1,4 +1,5 @@
 class Employee:
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -6,13 +7,20 @@ class Employee:
         self.pay = pay
         self.email = first+'.'+last+'@gmail.com'
 
-    def name(self):
+    def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
 
-emp1 = Employee('Nilesh','Pranami',200)
-emp2 = Employee('ram', 'kumar', 322)
+class Developer(Employee):
+    def __init__(self,first,last,pay,lang):
+        super().__init__(first,last,pay)
+        self.lang = lang
 
-print(Employee.name(emp1))
-print(emp1.email)
-print(emp2.email)
+dev1 = Developer('Nilesh','Pranami',200,'Python')
+dev2 = Developer('ram', 'kumar', 322,'Java')
+
+print(dev2.email)
+
+print(dev2.lang)
