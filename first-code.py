391ba08b0eb104d7c42d0e23cb52c888c3286d9a
Nilesh <nk966331@gmail.com>
2019-03-23 20:41:55 +0530

    Manager class added

--- first-code.py
+++ first-code.py
@@ -18,9 +18,38 @@ class Developer(Employee):
         super().__init__(first,last,pay)
         self.lang = lang
 
+class Manager(Employee):
+    def __init__(self, first, last, pay, employee = None):
+        super().__init__(first, last, pay)
+        if employee is None:
+            self.employee = []
+        else:
+            self.employee = employee
+
+
+    def addEmployee(self,emp):
+        if emp not in self.employee:
+            self.employee.append(emp)
+    
+    def removeEmployee(self, emp):
+        if emp in self.employee:
+            self.employee.remove(emp)
+
+    def printEmployee(self):
+        for emp in self.employee:
+            print('--> ',emp.fullname())
+
+
 dev1 = Developer('Nilesh','Pranami',200,'Python')
 dev2 = Developer('ram', 'kumar', 322,'Java')
+mng = Manager('Rahul', 'Pranami', 10000,[dev1])
+print(mng.email)
 
-print(dev2.email)
+mng.printEmployee()
 
-print(dev2.lang)
+mng.addEmployee(dev2)
+mng.printEmployee()
+
+mng.removeEmployee(dev2)
+mng.printEmployee()
+#print(dev2.lang)
