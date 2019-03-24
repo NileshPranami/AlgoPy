6e723ee4493e1854c145ba78e4f85271a4348d85
Nilesh <nk966331@gmail.com>
2019-03-24 18:56:45 +0530

    web scraping added

--- first-code.py
+++ first-code.py
@@ -1,19 +1,14 @@
-from matplotlib import pyplot as plt 
-from matplotlib import style
+import requests
+import bs4
+res=requests.get('https://en.wikipedia.org/wiki/Indian_Institute_of_Technology_Patna')
+type(res)
+res.text
+soup= bs4.BeautifulSoup(res.text,'lxml')
+type(soup)
+#hi=soup.select('.href')
 
-x = [5,8,10]
-y = [12,16,6]
-
-x2 = [6,9,11]
-y2 = [6,15,7]
-
-plt.plot(x,y,'g',label = 'line one',linewidth = 5)
-plt.plot(x2,y2,'c',label = 'line one',linewidth = 5)
-
-plt.title('info')
-plt.ylabel('y axis')
-plt.xlabel('x axis')
-
-plt.legend()
-plt.grid(True,color = 'k')
-plt.show()
+for link in soup.find_all('a', href=True):
+    if '#' in link['href']:
+        pass
+    else:
+        print(link['href'])
